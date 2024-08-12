9cf5f1e4eb77341d573dddd590f25a39d56671d1
Joaofelixz <joao.nascimento9@estudante.ifms.edu.br>
2024-08-12 16:10:05 -0400

    12/08

--- src/1-AtividadeCriarComponente/index.jsx
+++ src/1-AtividadeCriarComponente/index.jsx
@@ -5,7 +5,7 @@
 export default function Home() {
     return (
         <div>
-            <h1>Livros de Programação</h1>
+            <h1>lOJA DE CALÇADOS</h1>
         </div>
     );
 }
--- src/2-AtividadeCriarEstadoLista/index.jsx
+++ src/2-AtividadeCriarEstadoLista/index.jsx
@@ -2,5 +2,39 @@
 // Inicialize o estado com, no mínimo, 3 objetos, cada um contendo 3 propriedades, incluindo um `id` único.
 // Lembre-se de importar o `useState` do módulo `react` para utilizar o estado.
 
+export default function Home() {
+    return (
+        <div>
+            <h1>Livros de Programação</h1>
+        </div>
+    );
+}
+import { useState } from "react";
+
+export default function App(){
+    const [listadeProdutos, setlistadeProdutos] = useState([
+        {
+            id:1,
+
+            item:"tenis",
+
+            preco:"R$ 105,99"
+        },
+        {
+            id:2,
+
+            item:"tenis",
+
+            preco:"R$ 100,99"
+        },
+        {
+            id:3,
+
+            item:"tenis",
+
+            preco:"R$ 100,99"
+        },
+    ])
+}
 
 
