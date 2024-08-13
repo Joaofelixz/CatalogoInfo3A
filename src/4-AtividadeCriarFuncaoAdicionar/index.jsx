a0429a86ad9416d380cec11a792d558fdbe23cdc
Joaofelixz <159342228+Joaofelixz@users.noreply.github.com>
2024-08-13 15:46:42 -0400

    Atualizar o index.jsx

--- src/4-AtividadeCriarFuncaoAdicionar/index.jsx
+++ src/4-AtividadeCriarFuncaoAdicionar/index.jsx
@@ -3,3 +3,52 @@
 // A função recebe um parâmetro, que pode ser nomeado como `objeto`.
 // Dentro da função, utilize `setListaPedidos` para adicionar o `objeto` à `listaPedidos`.
 // Exemplo: `setListaPedidos([...listaPedidos, objeto]);`
+
+export default function Home() {
+  return (
+      <div>
+          <h1>LOJA DE CALÇADOS</h1>
+      </div>
+  );
+}
+import { useState } from "react";
+
+export default function App(){
+  const [listadeProdutos, setlistadeProdutos] = useState([
+      {
+          id:1,
+
+          item:"tenis",
+
+          preco:"R$ 105,99"
+      },
+      {
+          id:2,
+
+          item:"tenis",
+
+          preco:"R$ 100,99"
+      },
+      {
+          id:3,
+
+          item:"tenis",
+
+          preco:"R$ 100,99"
+      },
+  ])
+}
+
+
+  return (
+    <div className="Produtos">
+    {listadeProdutos.map((produtos)=>
+    <div key={produtos.id}>
+      <p>{produtos.item}</p>
+      <p>{produtos.preco}</p>
+    </div>
+  
+  )}
+    </div>
+   
+  );
