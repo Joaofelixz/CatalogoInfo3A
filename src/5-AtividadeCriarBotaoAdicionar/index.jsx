f2eae8416703abe25468c06fed4d2d4f0d75a98f
Joaofelixz <159342228+Joaofelixz@users.noreply.github.com>
2024-08-18 11:19:43 -0400

    Atualizar o index.jsx

--- src/5-AtividadeCriarBotaoAdicionar/index.jsx
+++ src/5-AtividadeCriarBotaoAdicionar/index.jsx
@@ -3,3 +3,51 @@
 // No valor do onClick, passe a função adicionarItemPedidos, 
 // por exemplo: onClick={() => adicionarItemPedidos(produto)}
 // No conteúdo do botão, coloque o texto "Selecionar" ou algo semelhante.
+
+import { useState } from "react";
+
+export default function Home(){
+    const [listadeProdutos, setlistadeProdutos] = useState([
+        {
+            id:1,
+
+            item:"tenis",
+
+            preco:"R$ 105,99"
+        },
+        {
+            id:2,
+
+            item:"tenis",
+
+            preco:"R$ 100,99"
+        },
+        {
+            id:3,
+
+            item:"tenis",
+
+            preco:"R$ 100,99"
+        },
+    ])
+}
+ const [listaPedidos, setlistaPedidos] = useState([]);
+   
+    const adicionarItemPedidos = (objeto) => {
+      setlistaPedidos([...listaPedidos, objeto])
+    }
+
+return (
+    <div>
+        <h1>Loja de Calçados</h1>
+        {
+          listaProdutos.map((produto)=>
+            <div key={produto.id}>
+              <p>{produto.item}</p>
+              <p>{produto.preco}</p>
+              <button onClick={()=>adicionarItemPedidos(produto)}>Selecionar</button>
+            </div>
+        )
+        }
+    </div>
+);
